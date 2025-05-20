a40dcf89a86db598cad17e5e5ca6172ccb0df4c2
nulledusername <lucho.jobly@gmail.com>
2025-05-20 17:31:00 -0300

    feat: update layout metadata and add Google Analytics integration; add new blog images for enhanced content

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Bravilo - Scouts de IA para negocios',
+  description:
+    'Bravilo automatiza y optimiza tareas repetitivas para liberar el potencial de tu negocio con scouts de IA personalizados.',
+  generator: 'braviloai.com',
 }
 
 export default function RootLayout({
@@ -13,8 +14,50 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-0ZSDZM57LM"
+        />
+        <Script strategy="afterInteractive" id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0ZSDZM57LM');
+          `}
+        </Script>
+
+        <script src="https://cdn.brevo.com/js/sdk-loader.js" async></script>
+<script>
+    // Version: 2.0
+    window.Brevo = window.Brevo || [];
+    Brevo.push([
+        "init",
+        {
+        client_key: "g44xy8xk1tyc66zvl16ihigd",
+        // Optional: Add other initialization options, see documentation
+        }
+    ]);
+</script>
+      </head>
+      <body>
+        {children}
+        <Script
+          type="module"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              import Chatbox from 'https://cdn.jsdelivr.net/npm/@bravilo/embeds@latest/dist/chatbox/index.js';
+              Chatbox.initBubble({
+                agentId: 'cm8rscw3d0000rj7wkb231fht',
+              });
+            `
+          }}
+        />
+      </body>
     </html>
   )
 }
